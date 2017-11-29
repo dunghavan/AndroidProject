1c3c6aa148f2a492db12464b608984dc56ab500f
dunghavan <dunghavan47@gmail.com>
2017-11-30 00:06:50 +0700

    Create FacebookLoginExample project

---
+++ FacebookLoginExample/app/src/main/java/com/example/dung/facebookloginexample/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.dung.facebookloginexample;
+//hash key: Y6GdAOm1lkSoWnONzJs3j5ZEnbw=
+//website: http://www.howkteam.vn/course/khoa-hoc-lap-trinh-android-co-ban/tich-hop-mang-xa-hoi--tao-app-facebook-1219
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
