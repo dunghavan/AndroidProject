49d42b6c0f9401786f0257a3460d43669f9b2464
dunghavan <dunghavan47@gmail.com>
2018-01-01 20:04:25 +0700

    Google login success

--- Demo_RecyclerView/app/build/generated/source/r/debug/android/support/v4/R.java
+++ Demo_RecyclerView/app/build/generated/source/r/debug/android/support/v4/R.java
@@ -67,36 +67,36 @@ public final class R {
         public static final int notify_panel_notification_icon_bg = 0x7f0200b6;
     }
     public static final class id {
-        public static final int action0 = 0x7f0d0144;
-        public static final int action_container = 0x7f0d0140;
-        public static final int action_divider = 0x7f0d0148;
-        public static final int action_image = 0x7f0d0141;
-        public static final int action_text = 0x7f0d0142;
-        public static final int actions = 0x7f0d0151;
+        public static final int action0 = 0x7f0d0147;
+        public static final int action_container = 0x7f0d0143;
+        public static final int action_divider = 0x7f0d014b;
+        public static final int action_image = 0x7f0d0144;
+        public static final int action_text = 0x7f0d0145;
+        public static final int actions = 0x7f0d0154;
         public static final int async = 0x7f0d0048;
         public static final int blocking = 0x7f0d0049;
-        public static final int cancel_action = 0x7f0d0145;
-        public static final int chronometer = 0x7f0d014d;
-        public static final int end_padder = 0x7f0d0153;
+        public static final int cancel_action = 0x7f0d0148;
+        public static final int chronometer = 0x7f0d0150;
+        public static final int end_padder = 0x7f0d0156;
         public static final int forever = 0x7f0d004a;
         public static final int icon = 0x7f0d0078;
-        public static final int icon_group = 0x7f0d0152;
-        public static final int info = 0x7f0d014e;
+        public static final int icon_group = 0x7f0d0155;
+        public static final int info = 0x7f0d0151;
         public static final int italic = 0x7f0d004b;
         public static final int line1 = 0x7f0d0007;
         public static final int line3 = 0x7f0d0008;
-        public static final int media_actions = 0x7f0d0147;
+        public static final int media_actions = 0x7f0d014a;
         public static final int normal = 0x7f0d001e;
-        public static final int notification_background = 0x7f0d014f;
-        public static final int notification_main_column = 0x7f0d014a;
-        public static final int notification_main_column_container = 0x7f0d0149;
-        public static final int right_icon = 0x7f0d0150;
-        public static final int right_side = 0x7f0d014b;
-        public static final int status_bar_latest_event_content = 0x7f0d0146;
+        public static final int notification_background = 0x7f0d0152;
+        public static final int notification_main_column = 0x7f0d014d;
+        public static final int notification_main_column_container = 0x7f0d014c;
+        public static final int right_icon = 0x7f0d0153;
+        public static final int right_side = 0x7f0d014e;
+        public static final int status_bar_latest_event_content = 0x7f0d0149;
         public static final int tag_transition_group = 0x7f0d0010;
         public static final int text = 0x7f0d0011;
         public static final int text2 = 0x7f0d0012;
-        public static final int time = 0x7f0d014c;
+        public static final int time = 0x7f0d014f;
         public static final int title = 0x7f0d0015;
     }
     public static final class integer {
--- Demo_RecyclerView/app/src/main/java/com/example/dung/demo_recyclerview/LoginActivity.java
+++ Demo_RecyclerView/app/src/main/java/com/example/dung/demo_recyclerview/LoginActivity.java
@@ -1,20 +1,38 @@
 package com.example.dung.demo_recyclerview;
 //hash key: Y6GdAOm1lkSoWnONzJs3j5ZEnbw=
 //website: http://www.howkteam.vn/course/khoa-hoc-lap-trinh-android-co-ban/tich-hop-mang-xa-hoi--tao-app-facebook-1219
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.dung.demo_recyclerview.model.Facebook_Profile;
 import com.facebook.*;
-import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInApi;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApi;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+
+//import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+
 
 import org.json.JSONObject;
 
@@ -22,7 +40,8 @@ import org.json.JSONObject;
  * Created by Dung on 12/3/2017.
  */
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener
+, View.OnClickListener{
     private static final String TAG = LoginActivity.class.getSimpleName();
     //private static boolean isAuthenticated = false;
     public static Facebook_Profile facebook_profile;
@@ -47,11 +66,32 @@ public class LoginActivity extends AppCompatActivity {
     public void setOnUpdateListener(OnUpdateListener listener){
         this.listener = listener;
     }
+
+    private static final int RC_SIGN_IN = 007;
+    private GoogleApiClient mGoogleApiClient;
+    private SignInButton  btn_googleLogin;
+    Button btn_googleLogout, btn_googleRevokeAccess;
+    private ProgressDialog mProgressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         activity = this; //Used to close Activity in static method
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+        btn_googleLogin = (SignInButton )findViewById(R.id.google_login_button);
+        btn_googleLogout = (Button)findViewById(R.id.google_logout_button);
+        btn_googleRevokeAccess = (Button)findViewById(R.id.google_revoke_access_button);
+        btn_googleLogin.setOnClickListener(this);
+        btn_googleLogout.setOnClickListener(this);
+        btn_googleRevokeAccess.setOnClickListener(this);
 
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.action_bar_layout);
@@ -63,6 +103,15 @@ public class LoginActivity extends AppCompatActivity {
 
         callbackManager = CallbackManager.Factory.create();
         facebook_profile = new Facebook_Profile();
+
+        SignInButton googleSignInButton = findViewById(R.id.google_login_button);
+        googleSignInButton.setSize(SignInButton.SIZE_STANDARD);
+        googleSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
 
         fbLoginButton = (LoginButton) findViewById(R.id.fb_login_button);
         //https://developers.facebook.com/docs/facebook-login/permissions#reference
@@ -93,6 +142,127 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        switch (id) {
+            case R.id.google_login_button:
+                signIn();
+                break;
+
+            case R.id.google_logout_button:
+                signOut();
+                break;
+
+            case R.id.google_revoke_access_button:
+                revokeAccess();
+                break;
+        }
+    }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    private void signOut() {
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+                new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(Status status) {
+                        updateUI(false);
+                    }
+                });
+    }
+
+    private void revokeAccess() {
+        Auth.GoogleSignInApi.revokeAccess(mGoogleApiClient).setResultCallback(
+                new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(Status status) {
+                        updateUI(false);
+                    }
+                });
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
+        if (opr.isDone()) {
+            // If the user's cached credentials are valid, the OptionalPendingResult will be "done"
+            // and the GoogleSignInResult will be available instantly.
+            Log.d(TAG, "Got cached sign-in");
+            GoogleSignInResult result = opr.get();
+            handleSignInResult(result);
+        } else {
+            // If the user has not previously signed in on this device or the sign-in has expired,
+            // this asynchronous branch will attempt to sign in the user silently.  Cross-device
+            // single sign-on will occur in this branch.
+            showProgressDialog();
+            opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
+                @Override
+                public void onResult(GoogleSignInResult googleSignInResult) {
+                    hideProgressDialog();
+                    handleSignInResult(googleSignInResult);
+                }
+            });
+        }
+    }
+    private void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage("Loading");
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    private void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.hide();
+        }
+    }
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+
+            Log.e(TAG, "display name: " + acct.getDisplayName());
+
+            String personName = acct.getDisplayName();
+            //String personPhotoUrl = acct.getPhotoUrl().toString();
+            String email = acct.getEmail();
+
+            Log.e(TAG, "Name: " + personName + ", email: " + email);
+
+            updateUI(true);
+        } else {
+            // Signed out, show unauthenticated UI.
+            updateUI(false);
+        }
+    }
+
+    private void updateUI(boolean isSignedIn) {
+        if (isSignedIn) {
+            btn_googleLogin.setVisibility(View.GONE);
+            btn_googleLogout.setVisibility(View.VISIBLE);
+            btn_googleRevokeAccess.setVisibility(View.VISIBLE);
+        } else {
+            btn_googleLogin.setVisibility(View.VISIBLE);
+            btn_googleLogout.setVisibility(View.GONE);
+            btn_googleRevokeAccess.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
+        // be available.
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
